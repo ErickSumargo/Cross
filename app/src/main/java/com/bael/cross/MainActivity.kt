2faffaf7685ae43234caaa7987403e89ef04d898
ErickSumargo <ericksumargo@outlook.com>
2020-10-30 11:58:50 +0700

    Setup Base Project (#2)

---
+++ app/src/main/java/com/bael/cross/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.bael.cross
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by ericksumargo on 01/10/20.
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

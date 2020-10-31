7577999e8be1184ba070fd4e190d95170542f196
Erick Sumargo <ericksumargo@outlook.com>
2020-10-31 22:14:37 +0700

    demo

---
+++ demo/src/main/java/com/bael/cross/demo/OpenModifierDemo.kt
@@ -0,0 +1,34 @@
+package com.bael.cross.demo
+
+/**
+ * Created by ericksumargo on 01/11/20.
+ */
+
+class OpenModifierDemo {
+
+    interface RandomGenerator {
+
+        fun generate(length: Int): String
+    }
+
+    open class TextRandom(val random: RandomGenerator) {
+
+        /**
+         * ...
+         *
+         * Some internal methods utilize the "generate" method.
+         */
+
+        open fun generate(length: Int): String {
+            return random.generate(length)
+        }
+    }
+
+    class TextNumericRandom(random: RandomGenerator) : TextRandom(random) {
+
+        override fun generate(length: Int): String {
+            val text = super.generate(length)
+            return text.replace("[^0-9]".toRegex(), "")
+        }
+    }
+}

35ff5c88bdd191399fcf92cf6f43ce5ced6b80e0
Erick Sumargo <ericksumargo@outlook.com>
2020-10-31 17:47:26 +0700

    demo

---
+++ demo/src/main/java/com/bael/cross/demo/UtilsNamingDemo.kt
@@ -0,0 +1,16 @@
+package com.bael.cross.demo
+
+/**
+ * Created by ericksumargo on 01/10/20.
+ */
+
+class UtilsNamingDemo {
+
+    object Util // or `Util`
+
+    object FileUtil
+
+    object Utils // or `Utils`
+
+    object FileUtils
+}
